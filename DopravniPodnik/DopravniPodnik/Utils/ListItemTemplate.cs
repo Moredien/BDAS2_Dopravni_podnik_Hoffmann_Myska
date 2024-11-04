f5b0c49646cb08ea09222848fa06bcc0a9a46265
Radek Hoffmann <radek.hoffmann@hotmail.com>
2024-11-04 02:33:42 +0100

    moved ListItemTemplate class to a new file

---
+++ DopravniPodnik/DopravniPodnik/Utils/ListItemTemplate.cs
@@ -0,0 +1,13 @@
+﻿namespace DopravniPodnik.Utils;
+
+public class ListItemTemplate
+{
+    public ListItemTemplate(string label, Type modelType)
+    {
+        Label = label;
+        ModelType = modelType;
+    }
+
+    public string Label { get; }
+    public Type ModelType { get; }
+}
