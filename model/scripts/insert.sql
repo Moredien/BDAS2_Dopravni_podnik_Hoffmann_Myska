79b7802f613f63077cb33776b3b4a8c624774207
Radek Hoffmann <radek.hoffmann@hotmail.com>
2024-11-08 21:15:10 +0100

    added insert and drop script

---
+++ model/scripts/insert.sql
@@ -0,0 +1,48 @@
+INSERT INTO TYPY_UZIVATELE (ID_TYP_UZIVATELE, NAZEV) VALUES (NULL, 'Admin');
+
+INSERT INTO TYPY_UZIVATELE (ID_TYP_UZIVATELE, NAZEV) VALUES (NULL, 'Zaměstnanec');
+
+INSERT INTO TYPY_UZIVATELE (ID_TYP_UZIVATELE, NAZEV) VALUES (NULL, 'Zákazník');
+
+insert into ADRESY (mesto, ulice, cislo_popisne) values ('Telêmaco Borba', 'John Wall', '54');
+insert into ADRESY (mesto, ulice, cislo_popisne) values ('Darhan', 'Victoria', '30568');
+insert into ADRESY (mesto, ulice, cislo_popisne) values ('Gaoqiao', 'Jay', '00');
+insert into ADRESY (mesto, ulice, cislo_popisne) values ('Baipu', 'Lerdahl', '17971');
+insert into ADRESY (mesto, ulice, cislo_popisne) values ('Artemisa', 'John Wall', '324');
+insert into ADRESY (mesto, ulice, cislo_popisne) values ('Granada', 'Lakeland', '2');
+insert into ADRESY (mesto, ulice, cislo_popisne) values ('Camperdown', 'Doe Crossing', '28');
+insert into ADRESY (mesto, ulice, cislo_popisne) values ('Gvozd', 'Carberry', '0223');
+insert into ADRESY (mesto, ulice, cislo_popisne) values ('Baumata', 'Express', '51');
+insert into ADRESY (mesto, ulice, cislo_popisne) values ('Ferkessédougou', 'Twin Pines', '5');
+insert into ADRESY (mesto, ulice, cislo_popisne) values ('Ash Shāmīyah', 'Crowley', '80906');
+insert into ADRESY (mesto, ulice, cislo_popisne) values ('San Diego', 'Hansons', '600');
+insert into ADRESY (mesto, ulice, cislo_popisne) values ('Khon San', 'Melvin', '01934');
+insert into ADRESY (mesto, ulice, cislo_popisne) values ('Hecun', 'Texas', '04910');
+insert into ADRESY (mesto, ulice, cislo_popisne) values ('Ranisoba', 'Pennsylvania', '831');
+insert into ADRESY (mesto, ulice, cislo_popisne) values ('Faya-Largeau', 'Burning Wood', '8366');
+insert into ADRESY (mesto, ulice, cislo_popisne) values ('Khiv', 'Schlimgen', '622');
+insert into ADRESY (mesto, ulice, cislo_popisne) values ('Tsimlyansk', 'Nobel', '8');
+insert into ADRESY (mesto, ulice, cislo_popisne) values ('Huangxi', 'Declaration', '1948');
+insert into ADRESY (mesto, ulice, cislo_popisne) values ('Nowe Miasto nad Pilicą', 'Jana', '9');
+
+
+insert into UZIVATELE (uzivatelske_jmeno, heslo, jmeno, prijmeni, cas_zalozeni, datum_narozeni, id_typ_uzivatele, id_adresy) values ('btruelock0', '$2a$04$.TiPndqY4Zt0lJlZYHpPcu0CG/QaUnb7Ynyk2xDP7Jjy7QywoVch.', 'Baily', 'Truelock', '27/06/2024', '06/04/2024', 2, 2);
+insert into UZIVATELE (uzivatelske_jmeno, heslo, jmeno, prijmeni, cas_zalozeni, datum_narozeni, id_typ_uzivatele, id_adresy) values ('rtrippitt1', '$2a$04$A0Q.lkK0jLiQCaGfHdzdK.3Q5rchDZlNszTHKeFIk0.ctZCdRGhY.', 'Rina', 'Trippitt', '07/08/2024', '10/03/2024', 3, 5);
+insert into UZIVATELE (uzivatelske_jmeno, heslo, jmeno, prijmeni, cas_zalozeni, datum_narozeni, id_typ_uzivatele, id_adresy) values ('ojablonski2', '$2a$04$HdOeXNw/WBK9Wy7S/2zHtuetJ7Cpj2uSgtZDpmvuKpYz1JurOaile', 'Othella', 'Jablonski', '20/08/2024', '23/01/2024', 2, 6);
+insert into UZIVATELE (uzivatelske_jmeno, heslo, jmeno, prijmeni, cas_zalozeni, datum_narozeni, id_typ_uzivatele, id_adresy) values ('elosebie3', '$2a$04$HeyWwbPizYY2wuPMRAG/ZO2YlgvzExQCTlc0oT881p37daWZt.p9O', 'Elana', 'Losebie', '12/05/2024', '31/07/2024', 3, 17);
+insert into UZIVATELE (uzivatelske_jmeno, heslo, jmeno, prijmeni, cas_zalozeni, datum_narozeni, id_typ_uzivatele, id_adresy) values ('clakin4', '$2a$04$YkcEQuIPKEr7Wi8ppvFWQe/Y4Wah1OC4LBydQjfywAZkWoYVZVGFy', 'Cindelyn', 'Lakin', '07/11/2024', '06/01/2024', 3, 13);
+insert into UZIVATELE (uzivatelske_jmeno, heslo, jmeno, prijmeni, cas_zalozeni, datum_narozeni, id_typ_uzivatele, id_adresy) values ('dduncan5', '$2a$04$k6.c8p4fJKkHw1HExTt3wu6BsJjFWknSsJO8JUZsabOD12gBJiy.y', 'Darrick', 'Duncan', '08/04/2024', '10/04/2024', 2, 9);
+insert into UZIVATELE (uzivatelske_jmeno, heslo, jmeno, prijmeni, cas_zalozeni, datum_narozeni, id_typ_uzivatele, id_adresy) values ('cffrenchbeytagh6', '$2a$04$vMnlf73NmwUtu7Dr1H1GUujJOapvaX60rwAhu8KY.LZLBfV09b7ES', 'Chicky', 'ffrench Beytagh', '17/07/2024', '28/02/2024', 1, 2);
+insert into UZIVATELE (uzivatelske_jmeno, heslo, jmeno, prijmeni, cas_zalozeni, datum_narozeni, id_typ_uzivatele, id_adresy) values ('jconnerry7', '$2a$04$ay5EpvWdQp1xgsZsB/GxWuxA6NXIHPoyChtThUZQIr90p8QZH9Qx2', 'Jackson', 'Connerry', '27/05/2024', '24/11/2023', 3, 1);
+insert into UZIVATELE (uzivatelske_jmeno, heslo, jmeno, prijmeni, cas_zalozeni, datum_narozeni, id_typ_uzivatele, id_adresy) values ('nblum8', '$2a$04$o/YpAXdE5WXraGvcwKylUepwUAmiXwogav9lFTgE/OpjjifTYxBaq', 'Noel', 'Blum', '21/06/2024', '19/12/2023', 2, 15);
+insert into UZIVATELE (uzivatelske_jmeno, heslo, jmeno, prijmeni, cas_zalozeni, datum_narozeni, id_typ_uzivatele, id_adresy) values ('morpyne9', '$2a$04$eGKj1/ZDmgyTUGi/GEqtD.NyX2yztCbf9WDiydxvcFnGYBQVtf4We', 'Massimo', 'Orpyne', '21/08/2024', '15/02/2024', 3, 14);
+insert into UZIVATELE (uzivatelske_jmeno, heslo, jmeno, prijmeni, cas_zalozeni, datum_narozeni, id_typ_uzivatele, id_adresy) values ('jjzaka', '$2a$04$v2eDCJZnAeFBq0fWWfhpSueoqJxSOXv3J64vOBHtyL.ZIAIR76c/2', 'Jandy', 'Jzak', '23/04/2024', '14/12/2023', 2, 17);
+insert into UZIVATELE (uzivatelske_jmeno, heslo, jmeno, prijmeni, cas_zalozeni, datum_narozeni, id_typ_uzivatele, id_adresy) values ('dcampab', '$2a$04$CLIRnZvBowuAcoPuTIAu..DQlBVYVtqtBAeDFdFvMdy80161n6sDG', 'Derrik', 'Campa', '14/11/2023', '03/01/2024', 3, 12);
+insert into UZIVATELE (uzivatelske_jmeno, heslo, jmeno, prijmeni, cas_zalozeni, datum_narozeni, id_typ_uzivatele, id_adresy) values ('gdooneyc', '$2a$04$NN3PY80..NZrF3dZaR.dHOx0QZbiF3QsAlbihnm8//qjOj3E9f.nu', 'Gaultiero', 'Dooney', '25/06/2024', '29/09/2024', 1, 3);
+insert into UZIVATELE (uzivatelske_jmeno, heslo, jmeno, prijmeni, cas_zalozeni, datum_narozeni, id_typ_uzivatele, id_adresy) values ('dlynthald', '$2a$04$UcIJP8fni2aja78TBIWJc.HNF3pGchUFiGW6SzssEvw.DCBYVVWiq', 'Dedra', 'Lynthal', '11/04/2024', '12/11/2023', 1, 9);
+insert into UZIVATELE (uzivatelske_jmeno, heslo, jmeno, prijmeni, cas_zalozeni, datum_narozeni, id_typ_uzivatele, id_adresy) values ('bkondrachenkoe', '$2a$04$Xzf7x1sFmW9q8IecPZUMBeGVhKqXfWQP6VeilOY67UA4vgrIU.ORK', 'Brodie', 'Kondrachenko', '11/09/2024', '08/03/2024', 1, 1);
+insert into UZIVATELE (uzivatelske_jmeno, heslo, jmeno, prijmeni, cas_zalozeni, datum_narozeni, id_typ_uzivatele, id_adresy) values ('gbookf', '$2a$04$PtdGwht8gQ3ZoGEhnlmwneKTdAWY6b7QFM.srkVXVEYRW.zzpBvLy', 'Gypsy', 'Book', '29/04/2024', '10/11/2023', 3, 7);
+insert into UZIVATELE (uzivatelske_jmeno, heslo, jmeno, prijmeni, cas_zalozeni, datum_narozeni, id_typ_uzivatele, id_adresy) values ('mrickaertg', '$2a$04$aBw0fSEmG73BGX4MTfa5auF7A2G9/5kzKx0ynR/l5q1fIewLjkOF.', 'Magdalen', 'Rickaert', '07/11/2024', '10/01/2024', 2, 20);
+insert into UZIVATELE (uzivatelske_jmeno, heslo, jmeno, prijmeni, cas_zalozeni, datum_narozeni, id_typ_uzivatele, id_adresy) values ('jmatitiahoh', '$2a$04$mRcORARUcwEc/uUbTtiZW.z3kJQdqxQgy/KRubfRS5lmj.0prT2FS', 'Jocelyne', 'Matitiaho', '27/12/2023', '13/10/2024', 3, 9);
+insert into UZIVATELE (uzivatelske_jmeno, heslo, jmeno, prijmeni, cas_zalozeni, datum_narozeni, id_typ_uzivatele, id_adresy) values ('cpriumi', '$2a$04$PPPqGIGzF48w73ehVPdVzOfaTDEQjviVt.gBFJiZjBeJBixZFieMC', 'Curtis', 'Prium', '28/10/2024', '25/08/2024', 2, 13);
+insert into UZIVATELE (uzivatelske_jmeno, heslo, jmeno, prijmeni, cas_zalozeni, datum_narozeni, id_typ_uzivatele, id_adresy) values ('ggladdinj', '$2a$04$eMJ.8kJiBYlJF/o4uZ0c8uCOSJbAs7YzGX/vPcyrFwLE3fnoACKRm', 'Gordan', 'Gladdin', '12/06/2024', '13/06/2024', 1, 1);
