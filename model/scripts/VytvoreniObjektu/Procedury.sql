de2aacd8404e37699ac03725c14ebe0b8e20febf
Radek Hoffmann <radek.hoffmann@hotmail.com>
2024-12-05 02:20:28 +0100

    Altered ZALOZIT_KARTU procedure and ZAMESTNANCI_VIEW

--- model/scripts/VytvoreniObjektu/Procedury.sql
+++ model/scripts/VytvoreniObjektu/Procedury.sql
@@ -6,18 +6,51 @@ CREATE OR REPLACE PROCEDURE ZALOZIT_KARTU (
     p_foto BLOB,
     p_jmeno_souboru VARCHAR2,
     p_typ_predplatneho VARCHAR2 DEFAULT NULL,
-    p_konec_predplatneho DATE DEFAULT NULL
+    p_konec_predplatneho DATE DEFAULT NULL,
+    p_vyse_platby NUMBER DEFAULT NULL,
+    -- Volitelné parametry pro platby kartou
+    p_cislo_karty VARCHAR2 DEFAULT NULL,
+    p_jmeno_majitele VARCHAR2 DEFAULT NULL,
+    -- Volitelné parametry pro platby převodem
+    p_cislo_uctu VARCHAR2 DEFAULT NULL
 )
 AS
     v_id_karty NUMBER;
+    v_id_foto NUMBER;
     v_id_typ_predplatneho NUMBER;
     v_od DATE := SYSDATE;
+    v_cas_platby DATE := SYSDATE;
+    v_id_platby NUMBER;
+    v_typ_platby NUMBER(1);
 BEGIN
 
+    -- Validace povinných dat pro foto
     IF p_foto IS NULL OR p_jmeno_souboru IS NULL THEN
         RAISE_APPLICATION_ERROR(-20004, 'Chybí informace o fotce. Foto i jméno souboru musí být zadány.');
     END IF;
+    
+    IF p_cislo_karty IS NOT NULL AND p_jmeno_majitele IS NOT NULL AND p_cislo_uctu IS NOT NULL THEN
+        RAISE_APPLICATION_ERROR(-20007, 'Nemůžou být zadány všechny parametry pro oba druhy plateb najednou.');
+    END IF;
 
+
+    -- Vložení záznamu do tabulky FOTO
+    INSERT INTO ST67028.FOTO (
+        JMENO_SOUBORU,
+        DATA,
+        DATUM_PRIDANI,
+        ID_KARTY,
+        ID_UZIVATELE
+    ) VALUES (
+                 p_jmeno_souboru,
+                 p_foto,
+                 SYSDATE,
+                 NULL,
+                 NULL -- Protože fotka je spojena s kartou a ne uživatelem
+             )
+    RETURNING ID_FOTO INTO v_id_foto;
+    
+    -- Vložení karty s přednastaveným ID_FOTO = NULL, bude se dělat update dále v proceduře
     INSERT INTO ST67028.KARTY_MHD (
         ZUSTATEK,
         PLATNOST_OD,
@@ -29,25 +62,15 @@ BEGIN
                  p_platnost_od,
                  p_platnost_do,
                  p_id_zakaznika,
-                 NULL
+                 v_id_foto
              )
     RETURNING ID_KARTY INTO v_id_karty;
+    
+    -- Aktualizace ID_KARTY u FOTO
+    UPDATE ST67028.FOTO
+    SET ID_KARTY = v_id_karty
+    WHERE ID_FOTO = v_id_foto;
 
-    INSERT INTO ST67028.FOTO (
-        JMENO_SOUBORU,
-        DATA,
-        DATUM_PRIDANI,
-        ID_KARTY,
-        ID_UZIVATELE
-    ) VALUES (
-                 p_jmeno_souboru,
-                 p_foto,
-                 SYSDATE,
-                 v_id_karty,
-                 NULL
-             );
-
-    -- Pokud je zadán typ předplatného, přidáme záznam o předplatném
     IF p_typ_predplatneho IS NOT NULL THEN
         SELECT ID_TYP_PREDPLATNEHO
         INTO v_id_typ_predplatneho
@@ -69,10 +92,68 @@ BEGIN
                      v_id_karty,
                      v_id_typ_predplatneho
                  );
+
+        IF p_vyse_platby IS NOT NULL THEN
+            IF p_cislo_karty IS NOT NULL AND p_jmeno_majitele IS NOT NULL THEN
+                v_typ_platby := 0; -- 0 pro platbu kartou
+
+                INSERT INTO ST67028.PLATBY (
+                    CAS_PLATBY,
+                    VYSE_PLATBY,
+                    ID_ZAKAZNIKA,
+                    TYP_PLATBY
+                ) VALUES (
+                             v_cas_platby,
+                             p_vyse_platby,
+                             p_id_zakaznika,
+                             v_typ_platby
+                         )
+                RETURNING ID_PLATBY INTO v_id_platby;
+
+                INSERT INTO ST67028.PLATBY_KARTOU (
+                    ID_PLATBY,
+                    CISLO_KARTY,
+                    JMENO_MAJITELE
+                ) VALUES (
+                             v_id_platby,
+                             p_cislo_karty,
+                             p_jmeno_majitele
+                         );
+
+            ELSIF p_cislo_uctu IS NOT NULL THEN
+                v_typ_platby := 1; -- 1 pro platbu prevodem
+
+                INSERT INTO ST67028.PLATBY (
+                    CAS_PLATBY,
+                    VYSE_PLATBY,
+                    ID_ZAKAZNIKA,
+                    TYP_PLATBY
+                ) VALUES (
+                             v_cas_platby,
+                             p_vyse_platby,
+                             p_id_zakaznika,
+                             v_typ_platby
+                         )
+                RETURNING ID_PLATBY INTO v_id_platby;
+
+                INSERT INTO ST67028.PLATBY_PREVODEM (
+                    ID_PLATBY,
+                    CISLO_UCTU
+                ) VALUES (
+                             v_id_platby,
+                             p_cislo_uctu
+                         );
+
+            ELSE
+                RAISE_APPLICATION_ERROR(-20006, 'Chybí informace o platbě. Musí být zadány parametry pro platbu kartou nebo převodem.');
+            END IF;
+        ELSE
+            RAISE_APPLICATION_ERROR(-20005, 'Chybí informace o výši platby.');
+        END IF;
     END IF;
 
     COMMIT;
-    
+
 EXCEPTION
     WHEN NO_DATA_FOUND THEN
         RAISE_APPLICATION_ERROR(-20002, 'Typ předplatného nebyl nalezen: ' || p_typ_predplatneho);
